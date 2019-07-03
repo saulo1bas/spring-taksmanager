215d4e3b022daf1192a5c9a6547b45243e140621
Saulo Bastos <saulo1bas@gmail.com>
2019-07-03 19:48:45 -0300

    Setting boostrap and jquery dependencies

--- src/main/java/com/saulo/gerenciadortarefas/controller/HomeController.java
+++ src/main/java/com/saulo/gerenciadortarefas/controller/HomeController.java
@@ -2,12 +2,16 @@ package com.saulo.gerenciadortarefas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String home() {
-		return "home/home";
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home/home");
+		mv.addObject("message","Controller Message");
+		return mv;
 	}	
 }
